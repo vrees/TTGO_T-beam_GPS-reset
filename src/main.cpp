9bdc96806a953cc012f7b94a45b75d20ceeefcda
vrees <viktor.rees@gmx.de>
2020-12-11 23:01:38 +0100

    added branch for restoring factory

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
-// #include <Arduino.h>
 /*************************************************************************
 Original sketch by LilyGO 
 https://github.com/LilyGO/TTGO-T-Beam/tree/master/GPS-T22_v1.0-20190612
 
 Modified by ErikThevking on August 25, 2020. Updated on August 27, 2020.
 
-Purpose:
-This sketch will bring back U-blox GPS N6M & N8M NMEA 9600 baud serial
-on TTGO T-beam T22- V1.0 and 1.1 and also on 0.7 T-beams (uncomment yours)
+Purpose: to reset U-blox NEO GPS devices 
+on TTGO T-beam T22- V1.0 and 1.1 and maybe on 0.7 T-beams (uncomment yours)
+This sketch will bring back U-blox GPS N6M & N8M factory settings 
+so that NMEA 9600 over the GPS serial output is enabled.
 
 Thanks to Kizniche for his advice on
 https://github.com/kizniche/ttgo-tbeam-ttn-tracker/issues/20
@@ -71,33 +71,79 @@ void setup()
   Serial.println("All comms started");
   delay(100);
 
-  do {
-    if (myGPS.begin(SerialGPS)) {
-      Serial.println("Connected to GPS");
-      myGPS.setUART1Output(COM_TYPE_NMEA); //Set the UART port to output NMEA only
-      myGPS.saveConfiguration(); //Save the current settings to flash and BBR
-      Serial.println("GPS serial connected, output set to NMEA");
-      myGPS.disableNMEAMessage(UBX_NMEA_GLL, COM_PORT_UART1);
-      myGPS.disableNMEAMessage(UBX_NMEA_GSA, COM_PORT_UART1);
-      myGPS.disableNMEAMessage(UBX_NMEA_GSV, COM_PORT_UART1);
-      myGPS.disableNMEAMessage(UBX_NMEA_VTG, COM_PORT_UART1);
-      myGPS.disableNMEAMessage(UBX_NMEA_RMC, COM_PORT_UART1);
-      myGPS.enableNMEAMessage(UBX_NMEA_GGA, COM_PORT_UART1);
-      myGPS.saveConfiguration(); //Save the current settings to flash and BBR
-      Serial.println("Enabled/disabled NMEA sentences");
-      break;
-    }
-    delay(1000);
-  } while(1);
-
 }  // endofsetup
+
 
 void loop()
 {
+    Serial.println();
+    Serial.print("===== STATE ");
+    Serial.println(state);
+    switch (state) {
+    case 0: // soft solution, should be sufficient and works in most (all) cases
+        do {
+          if (myGPS.begin(SerialGPS)) {
+           Serial.println("Connected to GPS");
+           myGPS.setUART1Output(COM_TYPE_NMEA); //Set the UART port to output NMEA only
+           myGPS.saveConfiguration(); //Save the current settings to flash and BBR
+           Serial.println("GPS serial connected, output set to NMEA");
+           myGPS.disableNMEAMessage(UBX_NMEA_GLL, COM_PORT_UART1);
+           myGPS.disableNMEAMessage(UBX_NMEA_GSA, COM_PORT_UART1);
+           myGPS.disableNMEAMessage(UBX_NMEA_GSV, COM_PORT_UART1);
+           myGPS.disableNMEAMessage(UBX_NMEA_VTG, COM_PORT_UART1);
+           myGPS.disableNMEAMessage(UBX_NMEA_RMC, COM_PORT_UART1);
+           myGPS.enableNMEAMessage(UBX_NMEA_GGA, COM_PORT_UART1);
+           myGPS.saveConfiguration(); //Save the current settings to flash and BBR
+           break;
+          }
+          delay(1000);
+        }  while(1);
+        Serial.println("Saved config");
+        state++;
+        break;
+        
+    case 1: // hardReset
+        Serial.println("Issuing hardReset (cold start)");
+        myGPS.hardReset();
+        delay(3000);
+        SerialGPS.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+        if (myGPS.begin(SerialGPS)) {
+            Serial.println("Success.");
+            state++;
+        } else {
+            Serial.println("*** GPS did not respond, starting over.");
+            state = 0;
+        }
+        break;
+        
+    case 2: // factoryReset, expect to see GPS back at 9600 baud
+        Serial.println("Issuing factoryReset");
+        myGPS.factoryReset();
+        delay(3000); // takes more than one second... a loop to resync would be best
+        SerialGPS.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+        if (myGPS.begin(SerialGPS)) {
+            Serial.println("Success.");
+            Serial.println("Now GPS has been reset with factory settings,");
+            Serial.println("give us some time to acquire satellites...");
+            Serial.println();
+            state++;
+        } else {
+            Serial.println("*** GPS did not respond, starting over.");
+            state = 0;
+        }
+        break;
 
-  if (SerialGPS.available()) {
-    Serial.write(SerialGPS.read());  // print anything comes in from the GPS
-  }
+    case 3: //
+        for (uint32_t thisNr = 1; thisNr < 300000000; thisNr++) {
+          if (SerialGPS.available()) {
+            Serial.write(SerialGPS.read());  // print anything comes in from the GPS
+            }
+          }
+
+
+  }  // endofswitchstate 
+
+
 
   
 }  // endofloop
